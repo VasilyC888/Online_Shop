3521fcc30e3315a1c91bc90da03785d28b19feea
Vasili Cassir <vasilicassir@MacBook-Pro-Vasili.local>
2023-01-20 21:54:50 +0200

    Added main structure and routing for app

---
+++ Shop/src/app/admin/admin.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import * as path from 'path';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { AdminLayoutComponent } from './shared/admin-layout/admin-layout.component';
+import { AddPageComponent } from './add-page/add-page.component';
+import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
+import { EditPageComponent } from './edit-page/edit-page.component';
+import { OrdersPageComponent } from './orders-page/orders-page.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild([
+      {
+        path: '', component: AdminLayoutComponent, children: [
+          { path: '', redirectTo: '/admin/login', pathMatch: 'full' },
+          { path: 'login', component: LoginPageComponent },
+          { path: 'dashboard', component: DashboardPageComponent },
+          { path: 'add', component: AddPageComponent },
+          { path: 'orders', component: OrdersPageComponent },
+          { path: 'product/:id/edit', component: EditPageComponent },
+        ]
+      },
+    ])
+  ],
+  exports: [RouterModule],
+  declarations: [
+    LoginPageComponent,
+    AdminLayoutComponent,
+    AddPageComponent,
+    DashboardPageComponent,
+    EditPageComponent,
+    OrdersPageComponent
+  ]
+})
+
+export class AdminModule {
+
+}
